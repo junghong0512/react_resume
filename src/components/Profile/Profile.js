2e0491226501a4d9396bfb5b50d01845e0bf027b
Jung <junghong91@naver.com>
2020-10-29 21:57:59 +0900

    Profile Component is done

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+
+import profileImg from "../../assets/images/profileImg.jpg";
+
+import "./Profile.css";
 
 const Profile = () => {
-  return <div>This is a profile component</div>;
+  return (
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className="name">정홍</Typography>
+        <Typography className="title">Fullstact Developer</Typography>
+      </div>
+
+      <figure className="profile_image">
+        <img src={profileImg} alt="profile" />
+      </figure>
+
+      <div className="profile_information">
+        Insert Timeline
+        <br />
+        <button>Contact</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
