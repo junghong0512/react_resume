2b54b4a7e5a54af4f000bb627b835cd58515f98f
Jung <junghong91@naver.com>
2020-11-06 12:32:09 +0900

    Contact form is done

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,7 +1,106 @@
 import React from "react";
+import { Grid, Icon, Paper, TextField, Typography } from "@material-ui/core";
+import EmailIcon from "@material-ui/icons/Email";
+
+import CustomButton from "../../components/Button/Button";
+import resumeData from "../../utils/resumeData";
+import "./Contact.css";
 
 const Contact = () => {
-  return <div>This is the Contact page</div>;
+  return (
+    <>
+      {/* Contact */}
+      <Grid container className="section pb_45 pt_45 mt_30 mb_30" spacing={6}>
+        {/* Contact form */}
+        <Grid item xs={12} lg={7}>
+          <Grid container>
+            <Grid item className="section_title mb_30">
+              <span></span>
+              <h6 className="section_title_text">Contact Form</h6>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                  <TextField fullWidth name="name" label="Name" />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField fullWidth name="email" label="Email" />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    name="message"
+                    label="Message"
+                    multiline
+                    rows={4}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <CustomButton text="Submit" icon={<EmailIcon />} />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* Contact information */}
+        <Grid item xs={12} lg={5}>
+          <Grid container>
+            <Grid item className="section_title mb_30">
+              <span></span>
+              <h6 className="section_title_text">Contact Information</h6>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Address: </span>
+                    {resumeData.address}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Phone: </span>
+                    {resumeData.phone}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Email: </span>
+                    {resumeData.email}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Birth: </span>
+                    {resumeData.birthdate}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography className="contactInfo_item">
+                    <span>Kakao: </span>
+                    {resumeData.kakao}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container className="contactInfo_socialsContainer">
+                {Object.keys(resumeData.socials).map((key, index) => (
+                  <Grid item className="contactInfo_social">
+                    <a href={resumeData.socials[key].link} about="_blank">
+                      {resumeData.socials[key].icon}
+                    </a>
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Contact;
